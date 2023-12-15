de54d9517f5bdffe02b96d9c00f5e6061c9185d2
VITALI BICOV <himaster@autodoc.eu>
2023-12-15 11:10:14 +0200

    add graceful exit for docker

--- main.py
+++ main.py
@@ -2,10 +2,19 @@
 
 import time
 import yaml
+import signal
 import requests
 
-from pprint import pprint
 from prometheus_client import start_http_server, Gauge
+
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self,signum, frame):
+    self.kill_now = True
 
 solana_health = Gauge("solana_health", "Solana node healthcheck", ["address"])
 
@@ -14,21 +23,28 @@ def healthcheck(server: str):
     response = requests.post(server, json=request)
 
     if response.status_code == 200:
-        pprint(response.json()['result'])
+        print(server + ' - ' + response.json()['result'])
         return 0
-    else:
-        pprint(response.json()['error']['message'])
+    elif 'numSlotsBehind' in response.json()['error']['data']:
+        print(response.json()['error']['message'])
         return response.json()['error']['data']['numSlotsBehind']
+    else:
+        print(response.json()['error']['message'])
+        return -1
 
 with open("config.yaml", "r") as yamlfile:
     data = yaml.load(yamlfile, Loader=yaml.FullLoader)
     print("Read successful")
 
 def main():
+    killer = GracefulKiller()
+
     while True:
         for server in data["solana_servers"]:
             result = healthcheck(server)
             solana_health.labels(address=server).set(result)
+        if killer.kill_now:
+            break
         time.sleep(5)
 
 if __name__ == '__main__':
