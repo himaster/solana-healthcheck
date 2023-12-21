63bf3119f4340b104fed1dbc4df57413f59b43ff
VITALI BICOV <himaster@autodoc.eu>
2023-12-21 12:27:23 +0200

    add wallet balancer monitoring

--- main.py
+++ main.py
@@ -17,13 +17,14 @@ class GracefulKiller:
     self.kill_now = True
 
 solana_health = Gauge("solana_health", "Solana node healthcheck", ["address", "server_group"])
+solana_wallet_balance = Gauge("solana_wallet_balance", "Solana wallet balance", ["address", "name"])
 
 def healthcheck(server: str):
     request = {"jsonrpc": "2.0", "id": 1, "method": "getHealth"}
     response = requests.post(server, json=request)
 
     if response.status_code == 200:
-        print(server + ' - ' + response.json()['result'])
+        #print(server + ' - ' + response.json()['result'])
         return 0
     elif 'numSlotsBehind' in response.json()['error']['data']:
         print(response.json()['error']['message'])
@@ -31,6 +32,16 @@ def healthcheck(server: str):
     else:
         print(response.json()['error']['message'])
         return -1
+
+def check_balance(wallet: str):
+    request =   {"jsonrpc": "2.0", "id": 1, "method": "getBalance",
+      "params": [
+        wallet
+      ]
+    }
+    response = requests.post('https://api.mainnet-beta.solana.com', json=request)
+
+    return int(response.json()["result"]["value"])/1000000000
 
 def main():
     killer = GracefulKiller()
@@ -41,6 +52,9 @@ def main():
                 for server in server_group["servers"]:
                     result = healthcheck(server)
                     solana_health.labels(address=server, server_group=server_group["group_name"]).set(result)
+            for wallet in data["wallets"]:
+                result = check_balance(wallet["value"])
+                solana_wallet_balance.labels(address=wallet["value"], name=wallet["name"]).set(result)
             if killer.kill_now:
                 break
             time.sleep(5)
